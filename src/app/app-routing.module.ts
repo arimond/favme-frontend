2d02c8edab4d0d955e3dae59efb679fbbed125a1
Peer Arimond <arimondp@hochschule-trier.de>
2020-07-14 10:38:15 +0200

    First Components and Angular Material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,69 @@
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
+import { PayoutComponent } from './components/user/payout/payout.component';
+import { ProfileComponent } from './components/user/profile/profile.component';
+import { DocumentComponent } from './components/user/document/document.component';
+import { ProductsComponent } from './components/user/products/products.component';
+import { UserComponent } from './components/user/user/user.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { LoginComponent } from './components/login/login.component';
+import { LandingComponent } from './components/landing/landing.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {
+    path: 'landing',
+    component: LandingComponent
+  },
+  {
+    path: 'register',
+    component: RegistrationComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'user',
+    component: UserComponent,
+    children: [
+      {
+        path: 'products',
+        component: ProductsComponent
+      },
+      {
+        path: 'documents',
+        component: DocumentComponent
+      },
+      {
+        path: 'profile',
+        component: ProfileComponent
+      },
+      {
+        path: 'payout',
+        component: PayoutComponent
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/landing',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: PagenotfoundComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(
+    routes,
+    {enableTracing: true} // Logs Router events to console for development
+    )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
+import { MaterialModule } from './shared/material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { UserComponent } from './components/user/user/user.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { ProfileComponent } from './components/user/profile/profile.component';
+import { PayoutComponent } from './components/user/payout/payout.component';
+import { DocumentComponent } from './components/user/document/document.component';
+import { ProductsComponent } from './components/user/products/products.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent,
+    UserComponent,
+    LandingComponent,
+    CustomerComponent,
+    ProfileComponent,
+    PayoutComponent,
+    DocumentComponent,
+    ProductsComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
